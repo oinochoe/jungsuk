4bdcfbf219921b49521a783bb7037461e9a337bd
oinochoe <oinochoe@naver.com>
2018-11-10 17:36:17 +0900

    9장 패키지와 유용한 클래스 진행중 -2.2 까지 완료

--- src/main/java/javastudy/jungsuk/ch_09/CloneEx2.java
+++ src/main/java/javastudy/jungsuk/ch_09/CloneEx2.java
@@ -8,7 +8,7 @@ public class CloneEx2 {
         /*int[] arrClone = new int[arr.length];                     // 배열을 생성하고
         System.arraycopy(arr, 0, arrClone, 0, arr.length);*/        // 내용을 복사한다.
         int[] arrClone = arr.clone(); // 위의 두줄을 한줄로 만든 부분.. 뜻은 같다.
-        // 배열 arr을 복제해서 같은 내용의 새로운 배열을 만든다.
+        // 배열 arr을 복제해서 같은 내용의 새로운 배열을 만든다.q
         arrClone[0] = 6;
 
         System.out.println(Arrays.toString(arr));
